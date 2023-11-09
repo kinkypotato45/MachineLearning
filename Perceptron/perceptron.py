8e819e8eeca11bad82b9740678f1171a613a484a
kinkypotato45 <u1175421@umail.utah.edu>
2023-11-08 22:42:20 -0700

    added testing file, implemented all methods

--- Perceptron/perceptron.py
+++ Perceptron/perceptron.py
@@ -3,7 +3,12 @@ A library implementing the perceptron algorithms. Ни пуха, ни пера!
 """
 import pandas as pd
 import numpy as np
+import copy
 from pandas.core.indexes.api import Axis
+
+frame = pd.read_csv("bank-note-1/train.csv", header=None)
+# print(frame)
+# print(len(frame))
 
 
 class Perceptron:
@@ -20,8 +25,6 @@ class Perceptron:
         """
         ones = pd.DataFrame({"bias": [1] * len(frame)})
         self.samples = ones.join(frame).to_numpy()
-        # self.classes = {}
-        # print(self.samples)
 
     def learn(self, r, epochs):
         self.w = [0] * (len(self.samples[0]) - 1)
@@ -48,13 +51,126 @@ class Perceptron:
         else:
             return self.returndict[1]
 
+    def returnWeights(self):
+        return self.w
 
-this = Perceptron(frame)
-this.learn(0.5, 1)
-frame = frame.to_numpy()
-for i in range(100):
-    row = frame[i]
-    x = row[:-1]
-    y = row[-1]
-    # print(frame[i])
-    print(this.predict(x), y)
+
+class VotedPerceptron:
+    def __init__(self, frame):
+        """
+        Takes in a dataframe, and create a linear classifier
+
+        Parameters
+        ----------
+        Frame: a pandas dataframe, where each column represents real value,
+        and the last column is a binary classfication.
+
+        r: the scale of how much to change weight vector w
+        """
+        ones = pd.DataFrame({"bias": [1] * len(frame)})
+        self.samples = ones.join(frame).to_numpy()
+        self.valuedict = {}
+        self.returndict = {}
+        self.WC = []
+        # self.classes = {}
+        # print(self.samples)
+
+    def learn(self, r, epochs):
+        self.w = [0] * (len(self.samples[0]) - 1)
+        # print(self.w)
+        yvalues = 1
+        c = 1
+        for _ in range(epochs):
+            for i in self.samples:
+                x = i[:-1]
+                y = i[-1]
+                if not self.valuedict.__contains__(y):
+                    self.valuedict[y] = yvalues
+                    self.returndict[yvalues] = y
+                    yvalues -= 2
+                if np.dot(self.w, x) * self.valuedict[y] <= 0:
+                    # print(self.w, c)
+                    self.WC.append((self.w.copy(), copy.copy(c)))
+                    self.w = self.w + r * x * self.valuedict[y]
+                    c = 1
+                else:
+                    c += 1
+        self.WC.append((self.w.copy(), copy.copy(c)))
+
+    def predict(self, input):
+        input = np.append([1], input)
+        value = 0
+        for weight, c in self.WC:
+            value += c * np.sign(np.dot(weight, input))
+
+        if value < 0:
+            return self.returndict[-1]
+        else:
+            return self.returndict[1]
+
+    def returnWeights(self):
+        return self.WC
+
+
+class AveragePerceptron:
+    def __init__(self, frame):
+        """
+        Takes in a dataframe, and create a linear classifier
+
+        Parameters
+        ----------
+        Frame: a pandas dataframe, where each column represents real value,
+        and the last column is a binary classfication.
+
+        r: the scale of how much to change weight vector w
+        """
+        ones = pd.DataFrame({"bias": [1] * len(frame)})
+        self.samples = ones.join(frame).to_numpy()
+        # self.classes = {}
+        # print(self.samples)
+
+    def learn(self, r, epochs):
+        self.w = [0] * (len(self.samples[0]) - 1)
+        self.a = [0] * (len(self.samples[0]) - 1)
+
+        # print(self.w)
+        yvalues = 1
+        self.valuedict = {}
+        self.returndict = {}
+        for _ in range(epochs):
+            for i in self.samples:
+                x = i[:-1]
+                y = i[-1]
+                if not self.valuedict.__contains__(y):
+                    self.valuedict[y] = yvalues
+                    self.returndict[yvalues] = y
+                    yvalues -= 2
+                if np.dot(self.w, x) * self.valuedict[y] <= 0:
+                    self.w = self.w + r * x * self.valuedict[y]
+                self.a = self.a + self.w
+
+    def predict(self, input):
+        input = np.append([1], input)
+        value = np.dot(self.a, input)
+
+        if value < 0:
+            return self.returndict[-1]
+        else:
+            return self.returndict[1]
+
+    def returnWeights(self):
+        return self.a
+
+
+#
+# this = VotedPerceptron(frame)
+# values = this.learn(0.5, 1)
+# frame = frame.to_numpy()
+# # count = 0
+# for i in range(100):
+#     row = frame[i]
+#     # print(row)
+#     x = row[:-1]
+#     y = row[-1]
+#     # print(frame[i])
+#     print(this.predict(x), y)
--- Perceptron/perceptrontest.py
+++ Perceptron/perceptrontest.py
@@ -2,16 +2,45 @@
 Test file for perceptron
 """
 import pandas as pd
-from perceptron import Perceptron
+from perceptron import Perceptron, VotedPerceptron, AveragePerceptron
+
 
 if __name__ == "__main__":
-    frame = pd.read_csv("bank-note-1/train.csv", header=None)
-    this = Perceptron(frame)
-    this.learn(0.5, 1)
+    frame = pd.read_csv("bank-note-1/test.csv", header=None)
+    ave = AveragePerceptron(frame)
+    vote = VotedPerceptron(frame)
+    perc = Perceptron(frame)
+    perc.learn(0.25, 10)
+    ave.learn(0.25, 10)
+    vote.learn(0.25, 10)
     frame = frame.to_numpy()
-    for i in range(100):
-        row = frame[i]
+    aveAccuracy = 0
+    voteAccuracy = 0
+    percAccuracy = 0
+    count = 0
+    for row in frame:
         x = row[:-1]
         y = row[-1]
-        # print(frame[i])
-        print(this.predict(x), y)
+        if ave.predict(x) == y:
+            aveAccuracy += 1
+        if vote.predict(x) == y:
+            voteAccuracy += 1
+        if perc.predict(x) == y:
+            percAccuracy += 1
+
+        count += 1
+    print("Base Perceptron accuracy, r = .25, epochs = 10:", percAccuracy / count)
+    print("Voted Perceptron accuracy, r = .25, epochs = 10:", voteAccuracy / count)
+    print("Average Perceptron accuracy, r = .25, epochs = 10:", aveAccuracy / count)
+    print(
+        "Base Perceptron final weight vector, r =.25, epochs = 10:",
+        perc.returnWeights(),
+    )
+    print("voted Perceptron final weight vectors and counts, r =.25, epochs = 10:")
+    vects = vote.returnWeights()
+    for weight, count in vects:
+        print(weight, count)
+    print(
+        "Averaged Perceptron final weight vector, r =.25, epochs = 10:",
+        ave.returnWeights(),
+    )
